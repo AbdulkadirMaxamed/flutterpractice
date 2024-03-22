cd4bf1c2a7bb2892a379d46c1624c36565215ef8
hackerabdul <abdulkadir.q12@gmail.com>
2024-03-22 20:44:58 +0000

    adding images and appBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello world!',
-          style: TextStyle(color: Colors.blue),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+         title: const Text('I Am Rich',
+           style: TextStyle(
+             color: Colors.white
+           ),
+         ),
+          backgroundColor: Colors.blueGrey[900],
         ),
+        body:
+            const Center(
+              child:
+                Image(
+                    image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/STS41B-35-1613_-_Bruce_McCandless_II_during_EVA_%28Retouched%29.jpg/800px-STS41B-35-1613_-_Bruce_McCandless_II_during_EVA_%28Retouched%29.jpg')
+                ),
+            ),
+            // Text('I AM IN THE CENTER',
+            //   style: TextStyle(
+            //     color: Colors.white
+            //   ),
+            // ),
       ),
     ),
   );
