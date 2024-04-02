7c9dbfce34db0c07e0c86505c916c0d454d5095c
hackerAbdul <abdulkadir.q12@gmail.com>
2024-04-02 01:23:05 +0100

    Completing xylophone challenge

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -11,10 +11,9 @@ void main(){
       home: Scaffold(
         appBar: AppBar(
           title: Text("Xylophone App"),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.brown,
         ),
         body: xylophoneApp(),
-        backgroundColor: Colors.teal,
       ),
     ),
   );
@@ -28,18 +27,118 @@ class xylophoneApp extends StatelessWidget {
 
     String name = "Abdulkadir";
 
-    return Center(
-      child: TextButton(
-        onPressed: (){
-          final player = AudioPlayer();
-          player.play(AssetSource('note1.wav'));
-        },
-        child: Text("$name", style:
-          TextStyle(
-            color: Colors.red
-          ),
+    return Row(
+      children: [
+        Column(
+          children: [
+            TextButton(
+              onPressed: (){
+                final player = AudioPlayer();
+                player.play(AssetSource('note1.wav'));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 50.0,
+                  width: 200.0,
+                  margin: const EdgeInsets.all(8.0),
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: (){
+                final player = AudioPlayer();
+                player.play(AssetSource('note2.wav'));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 50.0,
+                  width: 200.0,
+                  margin: const EdgeInsets.all(8.0),
+                  color: Colors.orange,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: (){
+                final player = AudioPlayer();
+                player.play(AssetSource('note3.wav'));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 50.0,
+                  width: 200.0,
+                  margin: const EdgeInsets.all(8.0),
+                  color: Colors.yellow,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: (){
+                final player = AudioPlayer();
+                player.play(AssetSource('note4.wav'));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 50.0,
+                  width: 200.0,
+                  margin: const EdgeInsets.all(8.0),
+                  color: Colors.green,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: (){
+                final player = AudioPlayer();
+                player.play(AssetSource('note5.wav'));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 50.0,
+                  width: 200.0,
+                  margin: const EdgeInsets.all(8.0),
+                  color: Colors.teal,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: (){
+                final player = AudioPlayer();
+                player.play(AssetSource('note6.wav'));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 50.0,
+                  width: 200.0,
+                  margin: const EdgeInsets.all(8.0),
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: (){
+                final player = AudioPlayer();
+                player.play(AssetSource('note7.wav'));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 50.0,
+                  width: 200.0,
+                  margin: const EdgeInsets.all(8.0),
+                  color: Colors.purple,
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
+      ],
     );
   }
 }
