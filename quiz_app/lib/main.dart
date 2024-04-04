3ec9836edc2b176367763c46c1502f0e4d95046b
hackerAbdul <abdulkadir.q12@gmail.com>
2024-04-05 00:45:21 +0100

    Completing preliminary concept of the quiz app, set up conditionals to check against question and create icon for right or wrong

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-      QuizApp()
+      const QuizApp()
   );
 }
 
@@ -20,33 +20,64 @@ class QuizApp extends StatefulWidget {
 class _QuizAppState extends State<QuizApp> {
 
 
-  List questions = ["hi","bye","try","why","lie"];
-
-  List<Icon> results = [
-    const Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    const Icon(
-      Icons.close,
-      color: Colors.red,
-    )
+  List<String> questionsList = [
+    "Sharks are mammal",
+    "Sea otters have a favorite rock they use to break open food.",
+    "The blue whale is the biggest animal to have ever lived."
   ];
 
-  String randomQuestionsGenerator(){
-    int randomNum = Random().nextInt(questions.length);
-    setState(() {
-      newQuestion = questions[randomNum];
-    });
-    print(newQuestion);
-    return newQuestion;
-  }
+  List<String> answerList = [
+    "False",
+    "True",
+    "True"
+  ];
+
+  int questionNumber = 0;
+
+  List<Icon> results = [
+
+  ];
+
+  // String randomQuestionsGenerator(){
+  //   int randomNum = Random().nextInt(questions.length);
+  //   setState(() {
+  //     newQuestion = questions[randomNum];
+  //   });
+  //   print(newQuestion);
+  //   return newQuestion;
+  // }
 
   Expanded btnBuilder(Color boxColor, String bool){
     return Expanded(
       child: TextButton(
           onPressed: (){
-            randomQuestionsGenerator();
+            //checks if answer is correct
+            if(answerList[questionNumber]==bool){
+              results.add(
+                  const Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  )
+              );
+            }else{
+              results.add(
+                  const Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  )
+              );
+            }
+
+            //iterates to the next question
+            if(questionNumber==questionsList.length-1){
+              setState(() {
+                questionNumber=0;
+              });
+            }else{
+              setState(() {
+                questionNumber++;
+              });
+            }
           },
           child: Container(
             color: boxColor,
@@ -62,7 +93,6 @@ class _QuizAppState extends State<QuizApp> {
       ),
     );
   }
-  String newQuestion = "First question";
 
   @override
   Widget build(BuildContext context) {
@@ -76,7 +106,7 @@ class _QuizAppState extends State<QuizApp> {
               Expanded(
                 flex: 3,
                 child: Center(
-                    child: Text(newQuestion,
+                    child: Text(questionsList[questionNumber],
                       style: const TextStyle(
                           color: Colors.white,
                           fontSize: 25.0
@@ -98,14 +128,3 @@ class _QuizAppState extends State<QuizApp> {
     );
   }
 }
-
-//do a key and value pair
-/*
-  questions = [
-    q.1: question
-    a.1: answer
-  ]
-
-  if q.1 and buttonpressed = a.1
-    then push Icon to the list of results
- */
