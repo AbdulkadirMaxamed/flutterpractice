505f1330b9148d71569756b6a3e3b12a50a2e657
hackerabdul <abdulkadir.q12@gmail.com>
2024-04-30 18:31:22 +0100

    error todo with api

--- weather_app/lib/screens/loading_screen.dart
+++ weather_app/lib/screens/loading_screen.dart
@@ -2,14 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
   @override
-  _LoadingScreenState createState() => _LoadingScreenState();
+  State<LoadingScreen> createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async{
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.lowest);
-    print(position);
+    print("here");
+    LocationPermission permission;
+    permission = await Geolocator.requestPermission();
+    print(permission);
+    if(permission == LocationPermission.always){
+      print("object");
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.lowest);
+      print(position);
+    }else{
+      throw Exception("Error");
+    }
   }
 
   @override
@@ -18,9 +29,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
+            print("hello");
             getLocation();
           },
-          child: Text('Get Location'),
+          child: const Text('Get Location'),
         ),
       ),
     );
