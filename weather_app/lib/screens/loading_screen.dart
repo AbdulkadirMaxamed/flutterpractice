14c7f22a7e8a23cf5bbd0366eb6be5d791a3198f
hackerAbdul <abdulkadir.q12@gmail.com>
2024-05-06 19:09:04 +0100

    using http requests to pull in data from openweather map

--- weather_app/lib/screens/loading_screen.dart
+++ weather_app/lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'location.dart';
+import 'package:http/http.dart' as http;
 
 
 class LoadingScreen extends StatefulWidget {
@@ -24,7 +24,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     print(location.latitude);
     print(location.longitude);
+  }
 
+  void getData(String location) async{
+    var path = '/data/2.5/weather';
+    var queryParam = {
+      'q':'London',
+      'units':'metric',
+      'appid':'267d8343e0dcfa47c2cacc02181b44f6'
+    };
+    var url = Uri.https('api.openweathermap.org', path, queryParam);
+    print(url);
+    // 267d8343e0dcfa47c2cacc02181b44f6
+    var response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    print(await http.read(Uri.https('example.com', 'foobar.txt')));
   }
 
   @override
@@ -33,7 +49,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            getLocation();
+            // getLocation();
+            getData('London');
           },
           child: const Text('Get Location'),
         ),
