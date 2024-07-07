336b8d4021fb45c9aae0573d23d7e618b39f7789
hackerabdul <abdulkadir.q12@gmail.com>
2024-07-07 21:36:39 +0100

    adding logout and loading for registering and loging in users

--- flash_chat/lib/screens/login_screen.dart
+++ flash_chat/lib/screens/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -17,79 +18,88 @@ class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
   String email="";
   String password="";
+  bool _showSpinner=false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: _showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: SizedBox(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            inputTextWidget(
-                hintText: 'Enter your email',
-                colour: Colors.blueAccent,
-                onChange: (value){
-                  email=value;
-                }
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            inputTextWidget(
-                hintText: 'Enter your password',
-                colour: Colors.lightBlueAccent,
-                onChange: (value){
-                  password=value;
-                }
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            btnWidget(
-              color: Colors.lightBlueAccent,
-              btnText: 'Log In',
-              onPress: ()async{
-                //log in functionality so some checks before opening the chat page
-                print("here");
-                try{
-                  final existingUser = await _auth.signInWithEmailAndPassword(
-                      email: email,
-                      password: password);
-                  print("success");
-                  print(existingUser);
-                  Navigator.pushNamed(context, ChatScreen.id);
-                }on FirebaseAuthException catch (e) {
-                  if (e.code == 'invalid-credential') {
-                    print('Invalid email/password');
-                  } else if (e.code == 'user-not-found') {
-                    print('An account with the following email does not exist.');
+              const SizedBox(
+                height: 48.0,
+              ),
+              inputTextWidget(
+                  hintText: 'Enter your email',
+                  colour: Colors.blueAccent,
+                  onChange: (value){
+                    email=value;
                   }
-                } catch (e) {
-                  print(e);
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              inputTextWidget(
+                  hintText: 'Enter your password',
+                  colour: Colors.lightBlueAccent,
+                  onChange: (value){
+                    password=value;
+                  }
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              btnWidget(
+                color: Colors.lightBlueAccent,
+                btnText: 'Log In',
+                onPress: ()async{
+                  setState(() {
+                    _showSpinner=true;
+                  });
+                  try{
+                    final existingUser = await _auth.signInWithEmailAndPassword(
+                        email: email,
+                        password: password
+                    );
+                    print("success");
+                    print(existingUser);
+                    Navigator.pushNamed(context, ChatScreen.id);
+                    setState(() {
+                      _showSpinner=false;
+                    });
+                  }on FirebaseAuthException catch (e) {
+                    if (e.code == 'invalid-credential') {
+                      print('Invalid email/password');
+                    } else if (e.code == 'user-not-found') {
+                      print('An account with the following email does not exist.');
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
                 }
-              }
-            ),
-            btnWidget(
-                color: Colors.blueAccent,
-                btnText: 'Back',
-                onPress: (){
-                  Navigator.pop(context);
-                }
-            ),
-          ],
+              ),
+              btnWidget(
+                  color: Colors.blueAccent,
+                  btnText: 'Back',
+                  onPress: (){
+                    Navigator.pop(context);
+                  }
+              ),
+            ],
+          ),
         ),
       ),
     );
--- flash_chat/lib/screens/registration_screen.dart
+++ flash_chat/lib/screens/registration_screen.dart
@@ -4,6 +4,7 @@ import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
@@ -17,82 +18,93 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
   String email = "";
   String password = "";
+  bool _showSpinner = false;
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag:'logo',
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            inputTextWidget(
-              hintText: 'Enter your email',
-              colour: Colors.blueAccent,
-              onChange: (value){
-                email=value;
-              }
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            inputTextWidget(
-              hintText: 'Enter your password',
-              colour: Colors.lightBlueAccent,
-              onChange: (value){
-                password = value;
-              }
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            btnWidget(
-              color: Colors.blueAccent,
-              btnText: 'Register',
-              onPress: ()async{
-                try {
-                  final credential = await _auth.createUserWithEmailAndPassword(
-                      email: email, password: password
-                  );
-                  if(credential !=null){
-                    Navigator.pushNamed(context, ChatScreen.id);
-                  }
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'weak-password') {
-                    print('The password provided is too weak.');
-                  } else if (e.code == 'email-already-in-use') {
-                    print('The account already exists for that email.');
-                  }
-                } catch (e) {
-                  print(e);
-                }
-
-              }
-            ),
-            TextButton(
-              onPressed: (){
-                Navigator.pop(context);
-              },
-              child: const Text('Already have an account? Login Here',
-              style: TextStyle(
-                  decoration: TextDecoration.underline
+      body: ModalProgressHUD(
+        inAsyncCall: _showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag:'logo',
+                child: SizedBox(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
                 ),
+              ),
+              const SizedBox(
+                height: 48.0,
+              ),
+              inputTextWidget(
+                hintText: 'Enter your email',
+                colour: Colors.blueAccent,
+                onChange: (value){
+                  email=value;
+                }
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              inputTextWidget(
+                hintText: 'Enter your password',
+                colour: Colors.lightBlueAccent,
+                onChange: (value){
+                  password = value;
+                }
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              btnWidget(
+                color: Colors.blueAccent,
+                btnText: 'Register',
+                onPress: ()async{
+                  setState(() {
+                    _showSpinner=true;
+                  });
+                  try {
+                    final  credential = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password
+                    );
+                    if(credential!=null){
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                    setState(() {
+                      _showSpinner=false;
+                    });
+
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'weak-password') {
+                      print('The password provided is too weak.');
+                    } else if (e.code == 'email-already-in-use') {
+                      print('The account already exists for that email.');
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
+
+                }
+              ),
+              TextButton(
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+                child: const Text('Already have an account? Login Here',
+                style: TextStyle(
+                    decoration: TextDecoration.underline
+                  ),
+                )
               )
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
