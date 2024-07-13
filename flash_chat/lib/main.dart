57d216d98fffc0c62fc4e000e4c3fa1d12cea966
hackerAbdul <abdulkadir.q12@gmail.com>
2024-07-13 20:05:52 +0100

    fixing firebase android issues

--- flash_chat/lib/main.dart
+++ flash_chat/lib/main.dart
@@ -4,10 +4,11 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(FlashChat());
 }
 
