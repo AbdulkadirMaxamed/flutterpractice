c4ef49a5a591e84d613befa4bc741e640bd53219
hackerabdul <abdulkadir.q12@gmail.com>
2024-07-16 13:37:52 +0100

    refactoring and adding functionality for when check boxes are updated using listview builder

--- todo_app/lib/widgets/task_list.dart
+++ todo_app/lib/widgets/task_list.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
 import 'task_tile.dart';
+import 'package:todo_app/modals/task.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
+
+  @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+
+  List<Task> tasks=[
+    Task(name: "Buy milk"),
+    Task(name: "Code"),
+    Task(name: "Call Stephen"),
+    Task(name: "Call Stephen"),
+    Task(name: "Call Stephen")
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index){
+        return TaskTile(
+          taskTitle: tasks[index].name,
+          isChecked: tasks[index].isDone,
+          checkedBoxCallBack: (bool? checkbox){
+            setState(() {
+              tasks[index].isDone=checkbox??false;
+              print(tasks[index].isDone);
+            });
+          }
+        );
+      },
+      itemCount: tasks.length,
     );
   }
 }
--- todo_app/lib/widgets/task_tile.dart
+++ todo_app/lib/widgets/task_tile.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/screens/tasks_screen.dart';
 
 class TaskTile extends StatelessWidget {
+  const TaskTile({super.key, required this.taskTitle, required this.isChecked, required this.checkedBoxCallBack});
+
+  final String taskTitle;
+  final bool isChecked;
+  final Function(bool?) checkedBoxCallBack;
 
   @override
   Widget build(BuildContext context) {
+
     return ListTile(
-      title: Text('Task 1'),
-      trailing: CheckedBox(),
+      title: Text(taskTitle,
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : TextDecoration.none
+        ),
+      ),
+      trailing: Checkbox(
+          activeColor: Colors.lightBlueAccent,
+          value: isChecked,
+          onChanged: checkedBoxCallBack
+        // toggleCheckBox: checkBoxState,
+      ),
     );
   }
 }
